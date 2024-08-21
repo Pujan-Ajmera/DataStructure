027cc7e4471a54cdce70d7e3403a58a1cbec01d7
Pujan-Ajmera <160005231+Pujan-Ajmera@users.noreply.github.com>
2024-08-21 15:37:09 +0530

    Add files via upload
    h index 2 done same as first one

---
+++ Leetcode/h_index2.java
@@ -0,0 +1,24 @@
+public class h_index2{
+	public static void main(String[] args) {
+		
+	}
+	public int hIndex(int[] citations) {
+        int n = citations.length;
+        int[] arr = new int[n+1];
+        for(int i=0;i<n;i++){
+            if(citations[i]>n){
+                arr[n]+=1;
+            }
+            else{
+                arr[citations [i]]+=1;
+            }
+        }
+        int co=0;
+        for(int i=n;i>=0;i--){
+            co+=arr[i];
+            if(i<=co) return i;
+        }
+
+        return -1;
+    }
+}
