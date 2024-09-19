e9b2fd4da102a8917d7f430615d9d4780e3e670e
Pujan-Ajmera <160005231+Pujan-Ajmera@users.noreply.github.com>
2024-09-19 15:05:48 +0530

    Create UpperBoundBS.java

---
+++ Leetcode/UpperBoundBS.java
@@ -0,0 +1,12 @@
+int findUpperBound(ArrayList<Integer> al, int target) {
+        int left = 0, right = al.size();
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (al.get(mid) <= target) {
+                left = mid + 1; // Move to the right half (includes mid)
+            } else {
+                right = mid; // Move to the left half
+            }
+        }
+        return left; // Index of the first element > target
+}
